b9b34ae57ad08fa79ae974bd8a3dc0b9d1d572ad
FutureXRP <the5blairsworld@gmail.com>
2026-03-24 13:37:51 -0500

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,7 +5,7 @@ import { NextResponse, type NextRequest } from 'next/server'
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
-  const next = requestUrl.searchParams.get('next') ?? '/dashboard'
+  const next = requestUrl.searchParams.get('next') ?? '/loading'
 
   if (code) {
     const cookieStore = await cookies()
